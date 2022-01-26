5e25e49c5625f6d3498fcd7e21f3548a6f5fe2e8
Victor Rivas <33326699+Rivaaas@users.noreply.github.com>
2022-01-26 15:50:39 -0300

    Avance 1.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState("");
+
+  useEffect(() => {
+      const consultarApi = async () => {
+        if (busqueda === "") return;
+        const imagenesPorPagina = 10;
+        const key = '25423740-9c00ad4b29bae6a57416f9373';
+        const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        console.log(resultado);
+      };
+      consultarApi();
+    }, [busqueda])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador De Imagenes</p>
+        <Formulario guardarBusqueda={guardarBusqueda} />
+      </div>
     </div>
   );
 }
