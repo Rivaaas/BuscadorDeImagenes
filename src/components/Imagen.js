e61284ba57f44638bde3022e75b3ca067b953b9a
Rivaaas <rivaaas.v@gmail.com>
2022-01-30 01:12:44 -0300

    Ya busca imagenes

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Imagen = () => {
+const Imagen = ({imagen}) => {
 
-//const = {imagen} = {}
+const {largeImageURL, likes, previewURL,tags,view }= imagen;
   //extraer las variables
-  return <p>Imagen</p>;
+  return (
+
+    <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
+      <div className='card'>
+        <img src={previewURL} alt='tags' className='card-img-top' />
+      </div>
+
+    </div>
+  );
 };
 
 export default Imagen;
